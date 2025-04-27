022cd7d81d7a9d70587a30c6eeaf6c5ffc932775
Alex Hall <alex@alexhall.org>
2025-04-27 16:31:47 -0500

    Back to generation only

--- src/LokiCat.Godot.R3.ObservableSignals/RxSignalAttribute.cs
+++
@@ -1,9 +0,0 @@
-﻿using System;
-
-namespace LokiCat.Godot.R3.ObservableSignals
-{
-    [AttributeUsage(AttributeTargets.Field)]
-    public sealed class RxSignalAttribute : Attribute
-    {
-    }
-}

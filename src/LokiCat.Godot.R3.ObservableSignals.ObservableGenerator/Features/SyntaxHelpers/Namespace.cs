4bf58f6ff28a18f224d7e3e49dfb1631c1fe5485
Alex Hall <alex@alexhall.org>
2025-04-29 20:15:39 -0500

    Fix namespace extraction

--- src/LokiCat.Godot.R3.ObservableSignals.ObservableGenerator/Features/SyntaxHelpers/Namespace.cs
+++ src/LokiCat.Godot.R3.ObservableSignals.ObservableGenerator/Features/SyntaxHelpers/Namespace.cs
@@ -1,15 +1,14 @@
 ﻿using System.Linq;
-using Microsoft.CodeAnalysis;
 using Microsoft.CodeAnalysis.CSharp.Syntax;
 
 namespace LokiCat.Godot.R3.ObservableSignals.ObservableGenerator.Features.SyntaxHelpers;
 
 public static class Namespace
 {
-    public static string GetNamespace(SyntaxNode node) {
-        return node.Ancestors()
-                   .OfType<BaseNamespaceDeclarationSyntax>()
-                   .FirstOrDefault()
-                   ?.Name.ToString() ?? "Global";
+    public static string GetNamespace(ClassDeclarationSyntax classDecl)
+    {
+        var ns = classDecl.Ancestors().OfType<BaseNamespaceDeclarationSyntax>().FirstOrDefault();
+
+        return ns?.Name.ToString() ?? "GlobalNamespace";
     }
 }

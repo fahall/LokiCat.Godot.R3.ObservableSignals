5be8245b11cf5fbd090f18e130be82146fd97d13
Alex Hall <alex@alexhall.org>
2025-04-30 19:49:00 -0500

    Build transitive for RxSignalAttribute.cs

---
+++ src/LokiCat.Godot.R3.ObservableSignals.ObservableGenerator/Features/Attributes/RxSignalAttribute.cs
@@ -0,0 +1,11 @@
+﻿using System;
+
+// ReSharper disable once CheckNamespace
+namespace LokiCat.Godot.R3.ObservableSignals
+{
+    [AttributeUsage(AttributeTargets.Field)]
+    public sealed class RxSignalAttribute : Attribute
+    {
+        public RxSignalAttribute() { }
+    }
+}

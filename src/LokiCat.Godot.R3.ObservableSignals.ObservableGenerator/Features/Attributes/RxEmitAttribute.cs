c8caee03a6bc6881c1a60af3167bb9595cc3389a
Alex Hall <alex@alexhall.org>
2025-05-07 21:42:30 -0500

    RxEmit instead of RxSignal for compatibility with SignalName generation

---
+++ src/LokiCat.Godot.R3.ObservableSignals.ObservableGenerator/Features/Attributes/RxEmitAttribute.cs
@@ -0,0 +1,7 @@
+﻿using System;
+
+namespace LokiCat.Godot.R3.ObservableSignals;
+
+[AttributeUsage(AttributeTargets.Delegate)]
+// ReSharper disable once UnusedType.Global
+public sealed class RxEmitAttribute : Attribute { }

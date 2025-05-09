47fd7c22963bbff28f8756a66560fe7767a37db2
Alex Hall <alex@alexhall.org>
2025-05-09 15:24:22 -0500

    Simplify to all generation based on [Signal]

--- src/LokiCat.Godot.R3.ObservableSignals.ObservableGenerator/Features/Attributes/RxEmitAttribute.cs
+++
@@ -1,7 +0,0 @@
-﻿using System;
-
-namespace LokiCat.Godot.R3.ObservableSignals;
-
-[AttributeUsage(AttributeTargets.Delegate)]
-// ReSharper disable once UnusedType.Global
-public sealed class RxEmitAttribute : Attribute { }
---
+++ src/LokiCat.Godot.R3.ObservableSignals.ObservableGenerator/Features/SyntaxHelpers/Types.cs
@@ -0,0 +1,10 @@
+﻿using Microsoft.CodeAnalysis;
+
+namespace LokiCat.Godot.R3.ObservableSignals.ObservableGenerator.Features.SyntaxHelpers;
+
+public static class Types
+{
+    public static string GetFullTypeName(this ITypeSymbol? symbol) =>
+        symbol?.ToDisplayString(SymbolDisplayFormat.FullyQualifiedFormat) ?? "object";
+
+}

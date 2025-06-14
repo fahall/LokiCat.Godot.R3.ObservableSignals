5135224d6c332b17fce0f48b8627e028b7d656e6
Alex Hall <alex@alexhall.org>
2025-06-13 23:56:24 -0500

    Inverse signal attribute

---
+++ src/LokiCat.Godot.R3.ObservableSignals.ObservableGenerator/Features/Generators/InverseSignalAttributeGenerator.cs
@@ -0,0 +1,27 @@
+﻿using System.Text;
+using Microsoft.CodeAnalysis;
+using Microsoft.CodeAnalysis.Text;
+
+namespace LokiCat.Godot.R3.ObservableSignals.ObservableGenerator.Features.Generators;
+
+public class InverseSignalAttributeGenerator
+{
+    // Injects a delegate‐level InverseSignalAttribute so no separate DLL is required
+    public static void EmitInverseSignalAttributeDefinition(GeneratorExecutionContext context)
+    {
+        const string code = @"
+using System;
+
+namespace LokiCat.Godot.R3.ObservableSignals.ObservableGenerator.Features.Generators
+{
+    [AttributeUsage(AttributeTargets.Delegate)]
+    internal sealed class InverseSignalAttribute : Attribute
+    {
+        public string OriginalName { get; }
+        public InverseSignalAttribute(string originalName) => OriginalName = originalName;
+    }
+}
+";
+        context.AddSource("InverseSignalAttribute.g.cs", SourceText.From(code, Encoding.UTF8));
+    }
+}
